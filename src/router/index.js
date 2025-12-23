1f94d6f9705b885dea888ae92ec36bd826b99e24
Li <2272267158@qq.com>
2025-12-23 14:48:24 +0800

    修复路由模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../views/Home.vue';
 import List from '../views/List.vue';
@@ -8,12 +8,12 @@ import User from '../views/User.vue';
 const routes = [
     {path: '/', component: Home},//首页
     {path: '/list', component: List},//列表
-    {path: '/detail', component: Detail},//详情
+    {path: '/detail/:id', component: Detail},//详情
     {path: '/user', component: User},//收藏
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
